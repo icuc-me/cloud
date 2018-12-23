f0aea927a8e1db23b1781ef25578977d2972b3c9
Chris Evich <chris-yewess@anonomail.me>
2018-12-22 22:24:24 -0500

    split terriform config + add backend setup
    Signed-off-by: Chris Evich <chris-cloud@icuc.me>

---
+++ terraform/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+    credentials = "${var.credentials}"
+    project = "${var.project}"
+    region = "${var.region}"
+    zone = "${var.zone}"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,24 +21,3 @@ variable "bucket" {
 variable "env_name" {
     description = "Name of environment to operating in (test, stage, prod)"
 }
-
-# From CLI -backend-config="backend.auto.tfvars"
-terraform {
-    backend "gcs" {}
-}
-
-provider "google" {
-    credentials = "${var.credentials}"
-    project = "${var.project}"
-    region = "${var.region}"
-    zone = "${var.zone}"
-}
-
-# Bucket for holding/locking terraform state
-resource "google_storage_bucket" "terraform-state" {
-    name = "${var.bucket}"
-    project = "${var.project}"
-    storage_class = "REGIONAL"
-    location = "${var.region}"
-}
-
