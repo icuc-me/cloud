0c57a6b075a9e4297c4db07fdcd056187d3f1840
Chris Evich <chris-yewess@anonomail.me>
2018-12-24 12:34:20 -0500

    Minor fix to image tagging
    Signed-off-by: Chris Evich <chris-yewess@anonomail.me>

--- bin/lib.sh
+++ bin/lib.sh
@@ -9,6 +9,8 @@ cd $SRC_DIR
 # Don't fail building layer 1 b/c make not yet installed
 IMAGE_NAME="$(make image_name 2> /dev/null || echo '')"
 VERSION="$(make version 2> /dev/null || echo '')"
+VERSION_MAJ_MIN="$(echo $VERSION | cut -d . -f 1-2)"
+VERSION_MAJ="$(echo $VERSION | cut -d . -f 1)"
 
 die() {
     echo -e "$1"
