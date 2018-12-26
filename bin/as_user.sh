06668ea5c15af47c22ab30fc23d47680d63bf05f
Chris Evich <chris-yewess@anonomail.me>
2018-12-26 11:09:47 -0500

    more verbose as_user script
    Signed-off-by: Chris Evich <chris-cloud@icuc.me>

--- bin/as_user.sh
+++ bin/as_user.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-set -ex
+set -e
 die() { echo -e "$2"; exit "$1"; }
 [[ -n "$AS_ID" ]] || die 2 'Expected \$AS_ID to be set.'
 [[ -n "$AS_USER" ]] || die 3 'Expected \$AS_USER to be set.'
 groupadd -g "$AS_ID" "$AS_USER"
 useradd -g "$AS_ID" -u "$AS_ID" "$AS_USER"
 set -x
+echo "Creating a working copy of source"
 rsync --quiet --stats --recursive --links \
     --safe-links --perms --sparse "--chown=$AS_ID:$AS_ID" \
     "/usr/src/" "/home/$AS_USER"
 cd "/home/$AS_USER"
+set -x
 exec sudo --set-home --user "$AS_USER" --login --stdin /usr/bin/bash -i -l -c "$@"
