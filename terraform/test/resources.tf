c22df5d8a6e32c074767bd124b80de02c89d304a
Chris Evich <chris-yewess@anonomail.me>
2018-12-30 23:36:05 -0500

    Setup VPC gateway addresses
    Signed-off-by: Chris Evich <chris-cloud@icuc.me>

--- terraform/test/resources.tf
+++ terraform/test/resources.tf
@@ -1,6 +1,15 @@
-# Bucket for holding/locking terraform state
-resource "google_storage_bucket" "test-bucket" {
-    name = "test-${var.bucket}"
-    project = "${var.project}"
-    force_destroy = "${var.env_name == "prod" ? false : true}"
+
+// ref: https://www.terraform.io/docs/providers/google/r/compute_address.html
+resource "google_compute_address" "gateway-external" {
+    name = "gateway-external-${var.env_uuid}-${count.index}"
+    address_type = "EXTERNAL"
+    description = "Static external address #${count.index + 1} for gateway into VPC"
+    network_tier = "${var.env_name != "prod" ? "STANDARD" : "PREMIUM"}"
+    count = "${var.env_name == "prod" ? 1 : 0}"  // save some money
+}
+
+resource "google_compute_address" "gateway-internal" {
+    name = "gateway-internal-${var.env_uuid}-${count.index}"
+    address_type = "INTERNAL"
+    description = "Static internal address #${count.index + 1} for the gateway out of VPC"
 }
