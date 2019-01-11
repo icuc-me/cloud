0dbf1fd7ed486a9ad9466d467caa2190714585e5
Chris Evich <chris-cloud.icuc.me>
2019-01-11 18:56:46 -0500

    realize strong-box bucket in test env
    Signed-off-by: Chris Evich <chris-cloud.icuc.me>

--- terraform/test/provider.tf
+++ terraform/test/provider.tf
@@ -1,6 +1,38 @@
+// Default
+
 provider "google" {
+    version = "~> 1.20"
     credentials = "${var.TEST_SECRETS["CREDENTIALS"]}"
     project = "${var.TEST_SECRETS["PROJECT"]}"
     region = "${var.TEST_SECRETS["REGION"]}"
     zone = "${var.TEST_SECRETS["ZONE"]}"
+}
+
+// Aliases
+
+provider "google" {
+    version = "~> 1.20"
+    alias = "test"
+    credentials = "${var.TEST_SECRETS["CREDENTIALS"]}"
+    project = "${var.TEST_SECRETS["PROJECT"]}"
+    region = "${var.TEST_SECRETS["REGION"]}"
+    zone = "${var.TEST_SECRETS["ZONE"]}"
+}
+
+provider "google" {
+    version = "~> 1.20"
+    alias = "stage"
+    credentials = "${var.STAGE_SECRETS["CREDENTIALS"]}"
+    project = "${var.STAGE_SECRETS["PROJECT"]}"
+    region = "${var.STAGE_SECRETS["REGION"]}"
+    zone = "${var.STAGE_SECRETS["ZONE"]}"
+}
+
+provider "google" {
+    version = "~> 1.20"
+    alias = "prod"
+    credentials = "${var.PROD_SECRETS["CREDENTIALS"]}"
+    project = "${var.PROD_SECRETS["PROJECT"]}"
+    region = "${var.PROD_SECRETS["REGION"]}"
+    zone = "${var.PROD_SECRETS["ZONE"]}"
 }
--- validate/smoke.sh
+++ validate/smoke.sh
@@ -7,7 +7,7 @@ source "$(dirname $0)/lib.sh"
 TF_TEST_DIRPATH="$SRC_DIR/terraform/test"
 RUNTIME_FILEPATH="$TF_TEST_DIRPATH/runtime.auto.tfvars"
 RUNTIME_KEYS="UUID ENV_NAME SRC_VERSION TEST_SECRETS"
-RUNTIME_SKEYS="UUID ENV_NAME SRC_VERSION CREDENTIALS SUSERNAME PROJECT REGION ZONE BUCKET UUID STRONGBOX STRONGKEY"
+RUNTIME_SKEYS="UUID CREDENTIALS SUSERNAME PROJECT REGION ZONE BUCKET UUID STRONGBOX STRONGKEY"
 
 indent 4 "Checking basic test env terraform target was executed"
 for fname in \
@@ -20,16 +20,16 @@ do
 done
 
 indent 4 "Checking expected runtime secrets present"
-for KEY in $RUNTIME_KEYS
+for KEY in ENV_NAME SRC_VERSION $RUNTIME_KEYS
 do
-    indent 5 "Checking $KEY"
-    egrep -q -a -m 1 "^$KEY = " $RUNTIME_FILEPATH | \
+    indent 5 "Checking $KEY is present"
+    egrep -q -a -m 1 "^$KEY = " $RUNTIME_FILEPATH || \
         die "Missing $KEY key in $RUNTIME_FILEPATH" 10
 done
 
 for KEY in $RUNTIME_SKEYS
 do
-    indent 5 "Checking $KEY"
-    egrep -q -a -m -1 "^    $KEY = \".+\"" $RUNTIME_FILEPATH | \
+    indent 5 "Checking nested $KEY is present"
+    egrep -q -a -m -1 "^    $KEY = \".+\"" $RUNTIME_FILEPATH || \
         die "Missing nested $KEY key in $RUNTIME_FILEPATH" 11
 done
