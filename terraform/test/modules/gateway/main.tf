c708c796980105a7b2d643e2393156829acdd2c2
Chris Evich <chris-cloud.icuc.me>
2019-01-15 10:56:15 -0500

    implement gateway module
    Signed-off-by: Chris Evich <chris-cloud.icuc.me>

---
+++ terraform/test/modules/gateway/main.tf
@@ -0,0 +1,74 @@
+
+variable "env_name" {
+    description = "Name of the current environment (test, stage, or prod)"
+}
+
+variable "env_uuid" {
+    description = "The UUID value associated with the current environment."
+}
+
+// ref: https://www.terraform.io/docs/providers/google/r/compute_address.html
+resource "google_compute_address" "gateway-ephemeral-ext" {
+    name = "gateway-ephemeral-${var.env_uuid}-0"
+    address_type = "EXTERNAL"
+    description = "Ephemeral external address for test gateway"
+    network_tier = "STANDARD"
+    count = "${var.env_name == "test" ? 1 : 0}"
+}
+
+resource "google_compute_address" "gateway-static-ext" {
+    name = "gateway-external-${var.env_uuid}-0"
+    address_type = "EXTERNAL"
+    description = "Static external address for production and staging gateway"
+    network_tier = "${var.env_name == "prod" ? "PREMIUM" : "STANDARD"}"  // save a little money
+    count = "${var.env_name != "test" ? 1 : 0}"  // use gateway-ephemeral for test
+}
+
+resource "google_compute_address" "gateway-internal" {
+    name = "gateway-internal-${var.env_uuid}-0"
+    address_type = "INTERNAL"
+    description = "Static internal address for gateway instance"
+}
+
+locals {
+    _gateway_nat_ip = "${concat(google_compute_address.gateway-ephemeral-ext.*.address,
+                                google_compute_address.gateway-static-ext.*.address)}"
+}
+
+// ref: https://www.terraform.io/docs/providers/google/d/datasource_compute_instance.html
+resource "google_compute_instance" "gateway-instance" {
+    name = "gateway-${var.env_uuid}-${count.index}"
+    machine_type = "f1-micro"
+    count = 1
+    description = "Gateway system for managing/routing traffic in/out of VPC"
+    boot_disk {
+        auto_delete = "true"
+        initialize_params {
+            size = 200
+            image = "centos-7"
+        }
+    }
+    can_ip_forward = "true"
+    network_interface {
+        network = "default"
+        network_ip = "${google_compute_address.gateway-internal.0.address}"
+    }
+    network_interface {
+        network = "default"
+        network_ip = "${google_compute_address.gateway-internal.1.address}"
+        access_config {
+            nat_ip = "${element(local._gateway_nat_ip, 0)}"
+            network_tier = "${var.env_name == "prod" ? "PREMIUM" : "STANDARD"}"
+        }
+    }
+}
+
+output "internal-gateway-link" {
+    value = "google_compute_instance.gateway-instance.self_link}"
+    sensitive = true
+}
+
+output "internal-gateway-ip" {
+    value = "${google_compute_instance.gateway-instance.network_interface.0.network_ip}"
+    sensitive = true
+}
--- terraform/test/resources.tf
+++ terraform/test/resources.tf
@@ -16,57 +16,11 @@ module "strong_unbox" {
     strongkey = "${var.TEST_SECRETS["STRONGBOX"]}"
 }
 
-/*  FIXME: testing
-
-// ref: https://www.terraform.io/docs/providers/google/r/compute_address.html
-resource "google_compute_address" "gateway-ephemeral-ext" {
-    name = "gateway-ephemeral-${var.UUID}-0"
-    address_type = "EXTERNAL"
-    description = "Ephemeral external address for test gateway"
-    network_tier = "STANDARD"
-    count = "${var.ENV_NAME == "test" ? 1 : 0}"
-}
-
-resource "google_compute_address" "gateway-static-ext" {
-    name = "gateway-external-${var.UUID}-0"
-    address_type = "EXTERNAL"
-    description = "Static external address for production and staging gateway"
-    network_tier = "${var.ENV_NAME == "prod" ? "PREMIUM" : "STANDARD"}"  // save a little money
-    count = "${var.ENV_NAME != "test" ? 1 : 0}"  // use gateway-ephemeral for test
-}
-
-resource "google_compute_address" "gateway-internal" {
-    name = "gateway-internal-${var.UUID}-0"
-    address_type = "INTERNAL"
-    description = "Static internal address for gateway instance"
-}
-
-locals {
-    _gateway_nat_ip = "${concat(google_compute_address.gateway-ephemeral-ext.*.address,
-                                google_compute_address.gateway-static-ext.*.address)}"
-}
-
-// ref: https://www.terraform.io/docs/providers/google/d/datasource_compute_instance.html
-resource "google_compute_instance" "gateway-instance" {
-    name = "gateway-${var.UUID}-${count.index}"
-    machine_type = "f1-micro"
-    count = 1
-    description = "Gateway system for managing/routing traffic in/out of VPC"
-    boot_disk {
-        auto_delete = "true"
-        initialize_params {
-            size = 200
-            image = "centos-7"
-        }
+module "gateway" {
+    source = "./modules/gateway"
+    providers {
+        google = "google"
     }
-    can_ip_forward = "true"
-    network_interface {
-        network = "default"
-        network_ip = "${google_compute_address.gateway-internal.address}"
-        access_config {
-            nat_ip = "${element(local._gateway_nat_ip, 0)}"
-            network_tier = "${var.ENV_NAME == "prod" ? "PREMIUM" : "STANDARD"}"
-        }
-    }
+    env_name = "${var.ENV_NAME}"
+    env_uuid = "${var.UUID}"
 }
-*/
