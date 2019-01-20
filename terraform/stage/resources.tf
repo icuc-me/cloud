6f5becdcea77995f7a1184bacd703a955a2108ea
Chris Evich <chris-cloud.icuc.me>
2019-01-20 14:24:03 -0500

    update stage from test
    Signed-off-by: Chris Evich <chris-cloud.icuc.me>

--- terraform/stage/resources.tf
+++ terraform/stage/resources.tf
@@ -1,28 +1,32 @@
 
 module "strongboxes" {
     providers = { google = "google" }
-    source = "../modules/strongboxes"
+    source = "./modules/strongboxes"
     env_name_uuid = "${var.UUID}"
     readers = {
-        test = ["${module.test_service_account.email}"],
-        stage = ["${module.test_service_account.email}"],
-        prod= []}
+        test = [],
+        stage = [],
+        prod= []
+    }
     strongbox = "${local.self["STRONGBOX"]}"
     strongkey = "${local.self["STRONGKEY"]}"
 }
 
-/*
+/* NOT READY YET
 module "strong_unbox" {
-    source = "../modules/strong_unbox"
+    source = "./modules/strong_unbox"
     credentials = "${local.self["CREDENTIALS"]}"
-    strongbox = "${var.ENV_NAME == "prod"
+    // Only way to add a dependency to a module block
+    strongbox = "${0 == 0
                    ? local.self["STRONGBOX"]
                    : module.strongboxes.uris[var.ENV_NAME]}"
-    strongkey = "${local.self["STRONGBOX"]}"
+    strongkey = "${local.self["STRONGKEY"]}"
 }
+*/
 
+/* NOT READY YET
 module "gateway" {
-    source = "../modules/gateway"
+    source = "./modules/gateway"
     providers {
         google = "google"
     }
