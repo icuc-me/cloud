03ad1d5f2a61f542747a297cb779cdd98a7a459b
Chris Evich <chris-cloud.icuc.me>
2019-04-14 13:26:56 -0400

    fix devel container sudoers
    Signed-off-by: Chris Evich <chris-cloud.icuc.me>

--- dockerfiles/as_user.sh
+++ dockerfiles/as_user.sh
@@ -22,7 +22,7 @@ if ! id "$AS_USER" &> /dev/null
 then
     groupadd -g "$AS_ID" "$AS_USER"
     useradd -g "$AS_ID" -u "$AS_ID" -G "wheel" "$AS_USER" --no-create-home  # assumed volume mount
-    echo '%wheel  ALL=(ALL)  NOPASSWD: ALL/' >> /etc/sudoers
+    echo '%wheel  ALL=(ALL)  NOPASSWD: ALL' >> /etc/sudoers
     install -o "$AS_ID" -g "$AS_ID" /etc/skel/.??* /home/$AS_USER
 fi
 
