229fa4f474d183160e7c3c8b558b1c9f0a6de649
Chris Evich <chris-cloud.icuc.me>
2019-04-19 20:51:49 -0400

    configure smtp server for use
    Signed-off-by: Chris Evich <chris-cloud.icuc.me>

--- ansible/roles/smtpserver/files/postconf_changed.sh
+++ ansible/roles/smtpserver/files/postconf_changed.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+set -e
+
+STRIP='s/^\s*(\S*)\s*$/\1/'
+
 DEFAULTS=$(postconf -d)
 postconf -n | while read ACTUAL
 do
-    KEY=$(echo "$ACTUAL" | cut -d = -f 1 | tr -d [:space:])
-    DEFAULT=$(echo "$DEFAULTS" | grep "^$KEY =")
-    [ "$ACTUAL" == "$DEFAULT" ] || echo "$ACTUAL"
+    KEY=$(echo "$ACTUAL" | cut -d "=" -f 1 | tr -d "[[:space:]]")
+    echo "$DEFAULTS" | egrep -q "^$KEY =" || continue
+
+    VALUE=$(echo "$ACTUAL" | cut -d "=" -f 2- | tr -s "[[:blank:]]" " " | sed -r -e "$STRIP")
+    DEFVAL=$(echo "$DEFAULTS" | egrep "^$KEY =" | cut -d "=" -f 2- | tr -s "[[:blank:]]" " " | sed -r -e "$STRIP")
+    if [[ "$VALUE" != "$DEFVAL" ]]
+    then
+	echo ""
+        echo "# DEFAULT $KEY: $DEFVAL"
+        echo "${KEY}: ${VALUE}"
+    fi
 done
