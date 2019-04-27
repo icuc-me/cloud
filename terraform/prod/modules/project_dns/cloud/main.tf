7c6b7dc4f17450f6f66ee05190ffe8ca6244eb8e
Chris Evich <chris-cloud.icuc.me>
2019-04-27 18:58:32 +0000

    roll stage into prod
    Signed-off-by: Chris Evich <chris-cloud.icuc.me>

--- terraform/prod/modules/project_dns/cloud/main.tf
+++ terraform/prod/modules/project_dns/cloud/main.tf
@@ -31,6 +31,10 @@ variable "project" {
     description = "Name of project managing these resources"
 }
 
+variable "env_name" {
+    description = "Name of the current environment (test, stage, prod)"
+}
+
 module "cloud" {
     source = "../sub"
     providers = {
@@ -41,10 +45,19 @@ module "cloud" {
     domain = "${var.domain}"
     base_zone = "${var.zone}"
     subdomain = "${var.cloud}"
+    create = "1"
 }
 
 locals {
     cloud_zone = "${module.cloud.name_to_zone[var.cloud]}"
+}
+
+resource "google_dns_record_set" "cloud_mx" {
+    managed_zone = "${local.cloud_zone}"
+    name = "${var.cloud}.${var.domain}."
+    type = "MX"
+    rrdatas = ["10 mail.${var.domain}."]
+    ttl = "300"
 }
 
 module "test" {
@@ -57,6 +70,7 @@ module "test" {
     domain = "${var.cloud}.${var.domain}"
     base_zone = "${local.cloud_zone}"
     subdomain = "test"
+    create = "1"
 }
 
 module "stage" {
@@ -69,6 +83,7 @@ module "stage" {
     domain = "${var.cloud}.${var.domain}"
     base_zone = "${local.cloud_zone}"
     subdomain = "stage"
+    create = "1"
 }
 
 module "prod" {
@@ -81,11 +96,12 @@ module "prod" {
     domain = "${var.cloud}.${var.domain}"
     base_zone = "${local.cloud_zone}"
     subdomain = "prod"
+    create = "1"
 }
 
 resource "google_dns_record_set" "gateway" {
     managed_zone = "${module.prod.name_to_zone["prod"]}"
-    name = "gateway.prod.${var.cloud}.${var.domain}."
+    name = "gateway.${var.env_name}.${var.cloud}.${var.domain}."
     type = "A"
     rrdatas = ["${var.gateway}"]
     ttl = 600
---
+++ terraform/prod/modules/project_dns/legacy/main.tf
@@ -0,0 +1,89 @@
+
+provider "google" {}
+
+variable "legacy_domains" {
+    description = "List of legacy FQDNs to manage with CNAMES into var.domain"
+    type = "list"
+}
+
+variable "domain" {
+    description = "FQDN for legacy domain's to point at"
+}
+
+variable "project" {
+    description = "Name of project managing these resources"
+}
+
+locals {
+    d = "."
+    h = "-"
+}
+
+// ref: https://www.terraform.io/docs/providers/google/r/dns_managed_zone.html
+resource "google_dns_managed_zone" "legacy" {
+    count = "${length(var.legacy_domains)}"
+
+    name = "${replace(var.legacy_domains[count.index], local.d, local.h)}"
+    dns_name = "${var.legacy_domains[count.index]}."
+    visibility = "public"
+    description = "Managed by terraform from project ${var.project}"
+}
+
+data "template_file" "names" {
+    count = "${length(var.legacy_domains)}"
+    template = "${element(split(local.d,
+                                google_dns_managed_zone.legacy.*.dns_name[count.index]),
+                          0)}"
+}
+
+resource "google_dns_record_set" "mx" {
+    count = "${length(var.legacy_domains)}"
+
+    managed_zone = "${google_dns_managed_zone.legacy.*.name[count.index]}"
+    name = "${google_dns_managed_zone.legacy.*.dns_name[count.index]}"
+    type = "MX"
+    rrdatas = ["10 mail.${var.domain}"]
+    ttl = "3600"
+}
+
+resource "google_dns_record_set" "mail" {
+    count = "${length(var.legacy_domains)}"
+
+    managed_zone = "${google_dns_managed_zone.legacy.*.name[count.index]}"
+    name = "mail.${google_dns_managed_zone.legacy.*.dns_name[count.index]}"
+    type = "CNAME"
+    rrdatas = ["mail.${var.domain}"]
+    ttl = "3600"
+}
+
+resource "google_dns_record_set" "www" {
+    count = "${length(var.legacy_domains)}"
+
+    managed_zone = "${google_dns_managed_zone.legacy.*.name[count.index]}"
+    name = "www.${google_dns_managed_zone.legacy.*.dns_name[count.index]}"
+    type = "CNAME"
+    rrdatas = ["www.${var.domain}"]
+    ttl = "3600"
+}
+
+resource "google_dns_record_set" "home" {
+    count = "${length(var.legacy_domains)}"
+
+    managed_zone = "${google_dns_managed_zone.legacy.*.name[count.index]}"
+    name = "home.${google_dns_managed_zone.legacy.*.dns_name[count.index]}"
+    type = "CNAME"
+    rrdatas = ["home.${var.domain}"]
+    ttl = "3600"
+}
+
+// add dependency on resource
+data "template_file" "zones" {
+    count = "${length(var.legacy_domains)}"
+    template = "${google_dns_managed_zone.legacy.*.name[count.index]}"
+}
+
+output "name_to_zone" {
+    value = "${zipmap(data.template_file.names.*.rendered,
+               data.template_file.zones.*.rendered)}"
+    sensitive = true
+}
