1cc10d805350ba84edb09c0739fe8a26d982def2
Chris Evich <chris-cloud.icuc.me>
2019-04-27 18:55:56 +0000

    roll test into stage
    Signed-off-by: Chris Evich <chris-cloud.icuc.me>

--- terraform/stage/modules/project_dns/sub/main.tf
+++ terraform/stage/modules/project_dns/sub/main.tf
@@ -24,6 +24,10 @@ variable "project" {
     description = "Name of project managing these resources"
 }
 
+variable "create" {
+    description = "Non-zero to create, 0 to skip"
+}
+
 locals {
     d = "."
     h = "-"
@@ -33,6 +37,7 @@ locals {
 
 // ref: https://www.terraform.io/docs/providers/google/r/dns_managed_zone.html
 resource "google_dns_managed_zone" "sub" {
+    count = "${var.create == 0 ? 0 : 1}"
     provider = "google.subdomain"
     name = "${local.name}"
     dns_name = "${local.fqdn}."
@@ -42,6 +47,7 @@ resource "google_dns_managed_zone" "sub" {
 
 // ref: https://www.terraform.io/docs/providers/google/r/dns_record_set.html
 resource "google_dns_record_set" "glue" {
+    count = "${var.create == 0 ? 0 : 1}"
     provider = "google.base"
     managed_zone = "${var.base_zone}"
     name = "${google_dns_managed_zone.sub.dns_name}"
@@ -50,8 +56,17 @@ resource "google_dns_record_set" "glue" {
     ttl = 86400
 }
 
+resource "google_dns_record_set" "mx" {
+    count = "${var.create == 0 ? 0 : 1}"
+    managed_zone = "${google_dns_managed_zone.sub.name}"
+    name = "${google_dns_managed_zone.sub.dns_name}"
+    type = "MX"
+    rrdatas = ["10 mail.${var.domain}"]
+    ttl = "3600"
+}
+
 output "name_to_zone" {
     // short-name to zone-name
-    value = "${map(var.subdomain, google_dns_managed_zone.sub.name)}"
+    value = "${map(var.subdomain, var.create == 0 ? "" : google_dns_managed_zone.sub.*.name[0])}"
     sensitive = true
 }
